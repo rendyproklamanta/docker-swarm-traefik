e73f6f43492ced19089fd714623e071272850faf
Rendy Dwi Proklamanta <rendydwi@myself.com>
2023-01-03 12:01:58 +0700

    update

--- Dockerfile.php
+++ Dockerfile.php
@@ -5,17 +5,14 @@ RUN apk add bash
 RUN apk add nginx
 RUN apk add php81 php81-fpm php81-opcache
 RUN apk add php81-gd php81-zlib php81-curl
+RUN mkdir -p /etc/nginx/conf.d
+RUN mkdir -p /var/run/php
 
 COPY nginx/nginx.conf /etc/nginx
 COPY nginx/conf.d/php.conf /etc/nginx/conf.d
 COPY php/conf /etc/php81
 COPY php/public /usr/share/nginx/html
 
-RUN mkdir /var/run/php
-
 EXPOSE 80
-EXPOSE 443
-
-STOPSIGNAL SIGTERM
 
 CMD ["/bin/bash", "-c", "php-fpm81 && chmod 777 /var/run/php/php81-fpm.sock && chmod 755 /usr/share/nginx/html/* && nginx -g 'daemon off;'"]
